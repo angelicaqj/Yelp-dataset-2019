3f454cbd7d478f542b8d9a6a88daaa273bff5482
angelicaqj <69495070+angelicaqj@users.noreply.github.com>
2021-05-10 16:29:48 -0700

    Update data_wrangling.py

--- data_wrangling.py
+++ data_wrangling.py
@@ -1,3 +1,50 @@
+import json
+import csv
+
+## Converting JSON to CSV
+
+# 1st dataset
+yelp_business = 'yelp_academic_dataset_business.json' 
+
+data = [] #empty list
+
+# opening JSON file and adding by line to the empty list
+for line in open(yelp_business, 'r'):
+    data.append(json.loads(line))
+
+print(data[0])
+
+# opening CSV file for writing 
+with open('yelp_business.csv', 'w') as file:
+
+    # create the CSV writer object
+    csv_business = csv.writer(file)
+    # writing headers to CSV file
+    csv_business.writerow(["business_id","name","city","state","postal_code","categories","stars"])
+
+    
+    for item in data:
+        csv_business.writerow([item["business_id"],item["name"],item["city"],item["state"],
+                               item["postal_code"],item["categories"],item["stars"]])
+
+# 2nd dataset with the same steps
+yelp_reviews = 'yelp_academic_dataset_review.json'
+data = []
+
+for line in open(yelp_reviews, 'r'):
+    data.append(json.loads(line))
+    
+with open('yelp_reviews.csv', 'w') as file:
+    csv_reviews = csv.writer(file)
+    csv_reviews.writerow(["review_id","business_id","stars","text","useful","funny","cool"])
+
+    for item in data:
+        csv_reviews.writerow([item["review_id"],item["business_id"],item["stars"],item["text"],
+                              item["useful"],item["funny"],item["cool"]])
+
+
+## Data Wrangling
+
 import pandas as pd
 
 # read business.csv data into a dataframe
